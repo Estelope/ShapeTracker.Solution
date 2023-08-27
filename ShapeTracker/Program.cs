773a2523b2730bd56c47612b6561628474a8baf8
Estelope <elopez14567@gmail.com>
2023-08-26 22:57:27 -0600

    functional checker

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -3,18 +3,17 @@ using ShapeTracker.Models;
 
 namespace ShapeTracker
 {
-  public class Program
+  class Program
   {
     static void Main()
     {
-      Triangle testTriangle = new Triangle();
-      Console.WriteLine(testTriangle.GetType());
-      // new code below
-      testTriangle.Side1 = 65;
-      testTriangle.Side3 = 88;
+      Triangle testTriangle = new Triangle(10,4,5);
       Console.WriteLine($"Side one of the triangle: {testTriangle.Side1}");
       Console.WriteLine($"Side two of the triangle: {testTriangle.Side2}");
       Console.WriteLine($"Side three of the triangle: {testTriangle.Side3}");
+      Console.WriteLine("Want to know what type of triangle you have?");
+      Console.WriteLine("Calculating...");
+      Console.WriteLine($"Your triangle is: {testTriangle.CheckType()}.");
     }
   }
 }
